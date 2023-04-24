8449e781f2a4599733575734f38b2066bb4ceac7
ksoderholm22 <115968278+ksoderholm22@users.noreply.github.com>
2023-04-23 20:55:46 -0500

    Update similo.py

--- similo.py
+++ similo.py
@@ -87,7 +87,7 @@ if selected=="Search":
         states=sorted(list(master['STATE_LONG'].astype(str).unique()))
         state_select=col1.multiselect('Filter Results by State(s)',states)
         count_select=col2.number_input(label='How many similar locations returned? (5-25)',min_value=5,max_value=25,value=10,step=5)
-        st.subheader('Data Category Importance (optional):')
+        st.subheader('Data Category Importance:')
         col1,col2,col3=st.columns([1,.7,.7])
         col1.caption('0.5 = Less Important')
         col2.caption('1.0 = Default')
