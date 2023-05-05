1c073af0fec70154ccc0bd752c1a9fbce3aae6cc
ksoderholm22 <115968278+ksoderholm22@users.noreply.github.com>
2023-05-05 15:19:32 -0500

    Update similo.py

--- similo.py
+++ similo.py
@@ -784,9 +784,11 @@ if selected=='About':
     st.divider()
     
     st.title('Creator')
-    st.write('**Name:**    Kevin Soderholm')
-    st.write('**Education:**    M.S. Applied Statistics')
-    st.write('**Experience:**    8 YOE in Data Science across Banking, Financial Technology, and Retail')
-    st.write('**Contact:**    kevin.soderholm@gmail.com or [linkedin](https://www.linkedin.com/in/kevin-soderholm-67788829/)')
-    st.write('**Thanks for stopping by!**')
+    col1,col2=st.columns(2)
+    col1.write('**Name:**    Kevin Soderholm')
+    col1.write('**Education:**    M.S. Applied Statistics')
+    col1.write('**Experience:**    8 YOE in Data Science across Banking, Fintech, and Retail')
+    col1.write('**Contact:**    kevin.soderholm@gmail.com or [linkedin](https://www.linkedin.com/in/kevin-soderholm-67788829/)')
+    col1.write('**Thanks for stopping by!**')
+    col2.image('kevin.png',width=300)
 
